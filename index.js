696be1d5d8d2e4b638037d3af79a4cba8dc0e898
aboodmagdy1 <c.aboodmagdy@gmail.com>
2023-09-25 11:07:00 +0300

    sync the tables

--- index.js
+++ index.js
@@ -2,18 +2,36 @@
 const app = require("express")();
 
 //custom library
+const Sequelize = require("sequelize");
 const sequelize = require("./config/databease");
 
-
-
+const Users = sequelize.define("user", {
+  id: {
+    type: Sequelize.DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  name: {
+    type: Sequelize.DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: Sequelize.DataTypes.STRING,
+  },
+  age: {
+    type: Sequelize.DataTypes.INTEGER,
+    defaultValue: 21,
+  },
+});
 
 sequelize
-  .authenticate()
-  .then(() => {
-    console.log("connected to database successfully");
+  .sync()
+  .then((data) => {
+    console.log('Table created successfully');
   })
-  .cathc((error) => {
-    console.log("error connecting to database");
+  .catch((err) => {
+    console.log('Error while creating tables')
   });
 
-console.log('another task ')
+console.log("another task ");
